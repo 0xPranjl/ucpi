0e08a383fe365d5e13564b60bdca48620f33c763
Pranjal Maurya <79030501+0xPranjl@users.noreply.github.com>
2022-09-28 20:00:37 +0530

    upgrade feature + id creation by signature
    remaining transfer mechanism ,setting price and custom brand mapping

--- ucpiv1.sol
+++ ucpiv1.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract ucpi{
+contract ucpinaming{
     //main ucpi mapping
     mapping(string=>string) public mainid;
     //check if wallet exist
@@ -16,17 +16,36 @@ contract ucpi{
     //number of wallet linked with id count
     mapping(string=>uint) public numofwallet;
     //mapping of id owner
-    mapping(string=>address) idowner;
+    mapping(string=>address) public idowner;
     //mapping of wallet owner address
-     mapping(string=>address) walletowner;
+     mapping(string=>address) public  walletowner;
      //mapping of wallet existance
-  
-    function createid(string memory _id,string memory _brand,string memory _address,string memory _walletname) external {
+    mapping(address=>uint) public walletownercount;
+    //number of ucpiid made by an address by default is 2 for more need to pay 151 harmony per id
+    mapping(address=>bool) public ispremium;
+    //account that has more than 2 ucpiid 
+    mapping(string=>uint) public idprice;
+    address public ucpimultisign=0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    function createid(string memory _id,string memory _brand,string memory _address,string memory _walletname,bytes memory sign) external {
         _id=validate(_id);
-        _brand=validate(_brand);
+          bytes32 wlcmhash=0x894717e02bc1017f7347665a198b3cb74671478407a02a65b750d7e88f8f8570;
+          bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(sign);
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix,wlcmhash));
+         
+        address wa=ecrecover(prefixedHashMessage, v, r, s);
+        
+        require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
         _walletname=validate(_walletname);
-        require(idexist[string(abi.encodePacked(_id,"@",_brand))]==false,"id not present");
-      //  require(&&idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=false,"wall")
+        
+         if(ispremium[wa]==false){
+           require(walletownercount[wa]<2,"free tier complete please purchase premium ");
+         }  
+         else{ 
+         }
+          
+         require(idexist[string(abi.encodePacked(_id,"@",_brand))]==false,"id not present");
+      //require(&&idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=false,"wall")
         mainid[string(abi.encodePacked(_id,"@",_brand))]=_address;
         walletsubwallet[string(abi.encodePacked(_id,"@",_brand))]=string(abi.encodePacked(_id,"@",_brand,"$",_walletname));
         numofwallet[string(abi.encodePacked(_id,"@",_brand))]+=1;
@@ -34,13 +53,17 @@ contract ucpi{
         idexist[string(abi.encodePacked(_id,"@",_brand))]=true;
         primarywallet[string(abi.encodePacked(_id,"@",_brand))]=_walletname;
         idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=true;
-        idowner[string(abi.encodePacked(_id,"@",_brand))]=msg.sender;
-        walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]==msg.sender;
+        idowner[string(abi.encodePacked(_id,"@",_brand))]=wa;
+        walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=wa;
+        walletownercount[wa]+=1;
+        ispremium[wa]=false;
+         
     }
     function updateaddress(string memory _id,string memory _brand,string memory _walletname,string memory _add) external {
         //bytes memory b=bytes(_address);
         _id=validate(_id);
         _brand=validate(_brand);
+         require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
         _walletname=validate(_walletname);
    require(walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]==msg.sender);
         wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_add;
@@ -49,6 +72,7 @@ contract ucpi{
      _id=validate(_id);
         _brand=validate(_brand);
         _walletname=validate(_walletname);
+         require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
      require(idowner[string(abi.encodePacked(_id,"@",_brand))]==msg.sender);
      require( idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]==false,"wallet name exists");
      wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_add;
@@ -60,6 +84,7 @@ contract ucpi{
          _id=validate(_id);
         _brand=validate(_brand);
         _walletname=validate(_walletname);
+          require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
          require(idowner[string(abi.encodePacked(_id,"@",_brand))]==msg.sender);
          require( idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=true,"wallet name exists");
           primarywallet[string(abi.encodePacked(_id,"@",_brand))]=_walletname;
@@ -93,4 +118,56 @@ contract ucpi{
         }
         return string(bLower);
     }
+ function recoverSigner(bytes memory _signature)
+        public
+        pure
+        returns (address)
+    {
+         // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+  //       (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+  // bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _ethSignedMessageHash));
+           bytes32 wlcmhash=0x894717e02bc1017f7347665a198b3cb74671478407a02a65b750d7e88f8f8570;
+          bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix,wlcmhash));
+         
+        return ecrecover(prefixedHashMessage, v, r, s);
+    }
+
+    function VerifyMessage(bytes32 _hashedMessage, uint8 _v, bytes32 _r, bytes32 _s) public pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _hashedMessage));
+        address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
+        return signer;
+    }
+   function splitSignature(bytes memory sig)
+        public
+        pure
+        returns ( bytes32 r,bytes32 s,uint8 v)   
+    {
+        require(sig.length == 65, "invalid signature length");
+            
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
+//     function get(string memory key) 
+//     public 
+//     view 
+//     returns (bytes32) {
+//         return bytes32(abi.encodePacked(key));
+// }
+function upgradeplan() external payable{
+  require(msg.value== 2000000000000000000,"please send 2 ");
+  payable(ucpimultisign).transfer(150000000);
+  ispremium[msg.sender]=true;
+}
+function bal() public view returns(uint){
+  return address(this).balance;
+}
+
+
 }
