a737003facf29cbf727e8e5ceea036aec55d42fd
Pranjal Maurya <79030501+0xPranjl@users.noreply.github.com>
2022-10-08 16:11:57 +0530

    fixed duplicate voting

--- ucpiv1.sol
+++ ucpiv1.sol
@@ -1,152 +1,41 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.7.0 <0.9.0;
-import "@openzeppelin/contracts/utils/Strings.sol";
-contract ucpinaming{
-    //main ucpi mapping
-    mapping(string=>string) public mainid;
-    //check if wallet exist
-    mapping(string=>bool) public idexist;
-    //primary wallet of id
-    mapping(string=>string) public primarywallet;
-    //wallet name address mapping 
-    mapping(string=>string) public wallet;
-    //sub wallets linked with id
-    mapping(string=>string) public walletsubwallet;
-    //number of wallet linked with id count
-    mapping(string=>uint) public numofwallet;
-    //mapping of id owner
-    mapping(string=>address) public idowner;
-    //mapping of wallet owner address
-     mapping(string=>address) public  walletowner;
-     //mapping of wallet existance
-    mapping(address=>uint) public walletownercount;
-    //number of ucpiid made by an address by default is 2 for more need to pay 151 harmony per id
-    mapping(address=>bool) public ispremium;
-    //account that has more than 2 ucpiid 
-    mapping(string=>uint) public idprice;
-    //resell price of the ucpi id
-    mapping(string=>string) public subwalletnum;
-    //number to subwallet mapping
-    mapping(uint=>string) public joiningnum;
-    //joining order
-    uint public usercount=0;
-    //number of user  
-      uint public resellcount=0;
-    //number of user  
-    address public ucpimultisign=0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-    function createid(string memory _id,string memory _brand,string memory _address,string memory _walletname,bytes memory sign,uint256 idp) external {
-        _id=validate(_id);
-           bytes32 wlcmhash=0x894717e02bc1017f7347665a198b3cb74671478407a02a65b750d7e88f8f8570;
-          bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        (bytes32 r, bytes32 s, uint8 v) = splitSignature(sign);
-        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix,wlcmhash));
-         
-        address wa=ecrecover(prefixedHashMessage, v, r, s);
-        
-        require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
-        _walletname=validate(_walletname);
-        
-         if(ispremium[wa]==false){
-           require(walletownercount[wa]<=1,"free tier complete please purchase premium ");
-         }  
-         else{ 
-         }
-          
-         require(idexist[string(abi.encodePacked(_id,"@",_brand))]==false,"id not present");
-      //require(&&idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=false,"wall")
-        mainid[string(abi.encodePacked(_id,"@",_brand))]=_address;
-        walletsubwallet[string(abi.encodePacked(_id,"@",_brand))]=string(abi.encodePacked(_id,"@",_brand,"$",_walletname));
-        numofwallet[string(abi.encodePacked(_id,"@",_brand))]+=1;
-        wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_address;
-        idexist[string(abi.encodePacked(_id,"@",_brand))]=true;
-        primarywallet[string(abi.encodePacked(_id,"@",_brand))]=_walletname;
-        idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=true;
-        idowner[string(abi.encodePacked(_id,"@",_brand))]=wa;
-        walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=wa;
-        idprice[string(abi.encodePacked(_id,"@",_brand))]=idp;
-        walletownercount[wa]+=1;
-       uint m=numofwallet[string(abi.encodePacked(_id,"@",_brand))];
-        subwalletnum[string(abi.encodePacked(_id,"@",_brand,"$",Strings.toString(m)))]=string(abi.encodePacked(_id,"@",_brand,"$",_walletname));
-        ispremium[wa]=false;
-        joiningnum[usercount]=string(abi.encodePacked(_id,"@",_brand));
-         usercount++;
-    }
-    function updateaddress(string memory _id,string memory _brand,string memory _walletname,string memory _add) external {
-        //bytes memory b=bytes(_address);
-        _id=validate(_id);
-        _brand=validate(_brand);
-         require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
-        _walletname=validate(_walletname);
-   require(walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]==msg.sender);
-        wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_add;
-    }
-    function addwallet(string memory _id,string memory _brand,string memory _walletname,string memory _add,address _ethadd) external{
-     _id=validate(_id);
-        _brand=validate(_brand);
-        _walletname=validate(_walletname);
-         require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
-     require(idowner[string(abi.encodePacked(_id,"@",_brand))]==msg.sender);
-     require( idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]==false,"wallet name exists");
-     wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_add;
-     walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=_ethadd;
-       walletsubwallet[string(abi.encodePacked(_id,"@",_brand))]=string(abi.encodePacked(walletsubwallet[string(abi.encodePacked(_id,"@",_brand))],",",_id,"@",_brand,"$",_walletname));
-     numofwallet[string(abi.encodePacked(_id,"@",_brand))]+=1;
-      uint m=numofwallet[string(abi.encodePacked(_id,"@",_brand))];
-        subwalletnum[string(abi.encodePacked(_id,"@",_brand,"$",Strings.toString(m)))]=string(abi.encodePacked(_id,"@",_brand,"$",_walletname));
-
-     
-    }
-    function changeprimary(string memory _id,string memory _brand,string memory _walletname,address _ethadd) external{
-         _id=validate(_id);
-        _brand=validate(_brand);
-        _walletname=validate(_walletname);
-          require(keccak256(abi.encodePacked("ucpi"))==keccak256(abi.encodePacked(_brand)),"no custom brands allowed");
-         require(idowner[string(abi.encodePacked(_id,"@",_brand))]==msg.sender);
-         require( idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=true,"wallet name exists");
-          primarywallet[string(abi.encodePacked(_id,"@",_brand))]=_walletname;
-          idowner[string(abi.encodePacked(_id,"@",_brand))]=_ethadd;
-    }
-    function validate(string memory s) public pure returns(string memory){
-     bytes memory bStr = bytes(s);
-        bytes memory bLower = new bytes(bStr.length);
-        for (uint i = 0; i < bStr.length; i++) {
-            // Uppercase character...
-            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
-                
-                bLower[i] = bytes1(uint8(bStr[i]) + 32);
-            }
-            else if((uint8(bStr[i]) >= 33) && (uint8(bStr[i]) <= 47)){
-              revert("special character not allowed");
-            } 
-            else if((uint8(bStr[i]) >= 58) && (uint8(bStr[i]) <= 64)){
-              revert("special character not allowed");
-            } 
-             else if((uint8(bStr[i]) >= 91) && (uint8(bStr[i]) <= 96)){
-              revert("special character not allowed");
-            } 
-             else if((uint8(bStr[i]) >= 123) && (uint8(bStr[i]) <= 127)){
-              revert("special character not allowed");
-            } 
-            
-            else {
-                bLower[i] = bStr[i];
-            }
-        }
-        return string(bLower);
-    }
- function recoverSigner(bytes memory _signature)
+contract verio {
+ 
+  struct node{
+      address nodeaddress;
+      string organisation;
+  }
+  node[] public nodes;
+  //address => hash(Jwt mapping)
+  struct vote{
+      uint yesvote;
+      uint novote;
+      
+  }
+  
+  mapping (address=>bytes32) public addjhash;
+  mapping (bytes32=>address) public jhashadd;
+  mapping (bytes32=>bytes32) public addidhash;
+  event Nodeadd(address nodeaddress,string organisation);
+  mapping (string=>bool) public nodereplacer;
+  mapping (string=>vote) public nodevote;
+  mapping (string=>bool) public ifvoted;
+  mapping (address=>string) public reversenodemap;
+  mapping (string=>address) public prenodeaddress;
+    constructor(string memory org){
+      nodes.push(node(msg.sender,org));
+      reversenodemap[msg.sender]=org;
+   }
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature)
         public
         pure
         returns (address)
     {
-         // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-  //       (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-  // bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _ethSignedMessageHash));
-           bytes32 wlcmhash=0x894717e02bc1017f7347665a198b3cb74671478407a02a65b750d7e88f8f8570;
           bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix,wlcmhash));
+  bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _ethSignedMessageHash));
          
         return ecrecover(prefixedHashMessage, v, r, s);
     }
@@ -170,51 +59,122 @@ contract ucpinaming{
             v := byte(0, mload(add(sig, 96)))
         }
     }
+ //getting total numbers of signature(number of signatures)
+ function tmp(bytes[] memory _signo)public pure returns(uint){
+  return _signo.length;
+ }
+ function genesisnode() public view returns(string memory){
+     return nodes[0].organisation;
+ }
+ function addnode(string memory organisation,bool _vote) external {
+     string memory _organisation=reversenodemap[msg.sender];
+     bytes memory isorgempty = bytes(_organisation); 
 
-//     function get(string memory key) 
-//     public 
-//     view 
-//     returns (bytes32) {
-//         return bytes32(abi.encodePacked(key));
-// }
-function upgradeplan() external payable{
-  require(msg.value== 151000000000000000000,"please send 2 ");
-  payable(ucpimultisign).transfer(150000000000000000000);
-  ispremium[msg.sender]=true;
+     require(isorgempty.length!=0,"you are not authorized to vote");
+     require(ifvoted[string(abi.encodePacked(organisation,"$",_organisation))]!=true,"already voted");
+    
+     if(_vote==false){
+          nodevote[organisation]=vote(nodevote[organisation].yesvote,nodevote[organisation].novote+1);
+      ifvoted[string(abi.encodePacked(organisation,"$",_organisation))]=true;
+     }
+     if(_vote==true){
+          nodevote[organisation]=vote(nodevote[organisation].yesvote+1,nodevote[organisation].novote);
+      ifvoted[string(abi.encodePacked(organisation,"$",_organisation))]=true;
+     }
+     if(nodevote[organisation].novote+nodevote[organisation].yesvote==nodes.length){
+         if(nodevote[organisation].yesvote>=nodevote[_organisation].novote){
+             
+               nodes.push(node(prenodeaddress[organisation],organisation));  
+               nodereplacer[organisation]=true;
+                reversenodemap[prenodeaddress[organisation]]=organisation;
+         }
+         else{
+          nodereplacer[organisation]=false;
+         }
+     }
+      
+ }
+ function nodeatindex(uint8 i) public view returns(string memory){
+     return nodes[i].organisation;
+ }
+function nodecount() public view returns(uint){
+     return nodes.length;
+ }
+
+function authrlogin(bytes32 jwthash) external{
+require(addidhash[jwthash] == 0,"jwt already used!");
+addjhash[msg.sender]=jwthash;
+addidhash[jwthash]=bytes32("none");
 }
-function bal() public view returns(uint){
-  return address(this).balance;
+function nodeaddreq(string memory organisation) external {
+for (uint x=0; x<nodes.length; x++) 
+{
+ if(keccak256(abi.encodePacked(nodes[x].organisation))==keccak256(abi.encodePacked(organisation))){
+ revert(); 
+ } 
 }
-function changeidprice(string memory _id,uint256 amount) external {
-require(msg.sender==walletowner[_id],"you are not authorized owner");
-idprice[_id]=amount;
+require(nodereplacer[organisation]==false,"organisation voting pending wait for result to reapply");
+emit Nodeadd(msg.sender,organisation);
+nodereplacer[organisation]=true;
+prenodeaddress[organisation]=msg.sender;
+}  
+
+function authrsign(bytes32 jwthash,bytes32 whois,bytes[] memory _verifysigno,bytes[] memory _whoissigno,bytes[] memory addo) external{
+require(_verifysigno.length==nodes.length,"Invalid number of verifier signature");
+require(_whoissigno.length==nodes.length,"Invalid number of whoissigno signature");
+ uint jwtvote;
+ uint whovote;
+ for(uint i=0; i<nodes.length; i++){
+        if(nodes[i].nodeaddress==recoverSigner(jwthash, _verifysigno[i])){
+            jwtvote++;
+        }
+         if(nodes[i].nodeaddress==recoverSigner(whois, _whoissigno[i])){
+            whovote++;
+        }
+        
+     }
+     require(jwtvote>(nodes.length/2),"auth failed jwtvote");
+        require(whovote>(nodes.length/2),"auth failed whovote");
+
+        addidhash[jwthash]=whois;
 }
-function buyid(string memory _id,string memory add,string memory _walletname,uint amnt ) external payable{
-  require(msg.value==idprice[_id],"please send amount set by owner");
-  string memory _brand="ucpi";
-     require(idexist[string(abi.encodePacked(_id,"@","ucpi"))]==false,"id not present");
-      //require(&&idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=false,"wall")
-         payable(ucpimultisign).transfer(idprice[_id]/10);
-          payable( idowner[string(abi.encodePacked(_id,"@",_brand))]).transfer(8*(idprice[_id]/10));
-        mainid[string(abi.encodePacked(_id,"@",_brand))]="";
-        walletsubwallet[string(abi.encodePacked(_id,"@",_brand))]="";
-        primarywallet[string(abi.encodePacked(_id,"@",_brand))]=_walletname;
-        idexist[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=true;
-        idowner[string(abi.encodePacked(_id,"@",_brand))]=msg.sender;
-        mainid[string(abi.encodePacked(_id,"@",_brand))]=add;
-        walletowner[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]=msg.sender;
-        idprice[string(abi.encodePacked(_id,"@",_brand))]=amnt;
-        walletownercount[msg.sender]+=1;
-        ispremium[msg.sender]=false;
-        uint x=numofwallet[string(abi.encodePacked(_id,"@",_brand))];
-         for(uint i=0; i<x; i++){
-       
-        string memory swa=subwalletnum[string(abi.encodePacked(_id,"@",_brand,"$",Strings.toString(i)))];
-        walletowner[swa]=msg.sender;
-     wallet[string(abi.encodePacked(_id,"@",_brand,"$",_walletname))]="";
-         }  
-     numofwallet[string(abi.encodePacked(_id,"@",_brand))]=0;
-     resellcount++;
+function op(string memory organisation) external view returns(uint){
+  return nodevote[organisation].yesvote;
+
 }
+/* just for testing recoverSigner testing
+function authsign1(bytes32 jwthash,bytes32 whois,bytes[] memory _verifysigno,bytes[] memory _whoissigno) external  pure returns(address){
+ address na;
+        na=recoverSigner(jwthash, _verifysigno[0]);
+     return na;
+}
+function authsign2(bytes32 jwthash,bytes32 whois,bytes[] memory _verifysigno,bytes[] memory _whoissigno) external  pure returns(address){
+ address na;
+        na=recoverSigner(jwthash, _verifysigno[1]);
+     return na;
+}
+function authsign3(bytes32 jwthash,bytes32 whois,bytes[] memory _verifysigno,bytes[] memory _whoissigno) external  pure returns(address){
+ address na;
+        na=recoverSigner(whois, _whoissigno[0]);
+     return na;
+}
+function authsign4(bytes32 jwthash,bytes32 whois,bytes[] memory _verifysigno,bytes[] memory _whoissigno) external  pure returns(address){
+ address na;
+        na=recoverSigner(whois, _whoissigno[1]);
+     return na;
+}
+function au(bytes32 jw,bytes[] memory x) external returns(address){
+address na;
+        na=recoverSigner(jw,x[0]);
+     return na;
+}
+    function mv(bytes32 _hashedMessage, uint8 _v, bytes32 _r, bytes32 _s) public pure returns (address) {
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _hashedMessage));
+        address signer = ecrecover(prefixedHashMessage, _v, _r, _s);
+        return signer;
+    }
+    */
+
 
 }
